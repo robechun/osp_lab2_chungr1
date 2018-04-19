3bf696aaa147e332ea7eba79bdeea97eef0d0edd
Chung <chungr1@debian-9.3.0-template>
2018-04-19 14:26:21 -0700

    Polishing up redundant code, adding better comments

--- new_tail.c
+++ new_tail.c
@@ -10,70 +10,33 @@ typedef struct lineNode {
 } line_node;
 
 // Function prototypes
-void deallocate(line_node *);
 line_node* deallocateLineNode(line_node *);
 
 int main(int argc, char*argv[]) {
-	// Initialize variables to use for getline() and fopen()
-	// TODO: add better comments
-	FILE *fs;
-	char *line = NULL;
-	size_t len = 0;
-	ssize_t nread;
-	size_t linesToRead = 5;
-	size_t linesRead = 0;
-	line_node *head = malloc(sizeof(line_node));
-	line_node *cur = head;
+	FILE *fs;											// File to be opened for write
+	char *line = NULL;									// line used for getline
+	size_t len = 0;										// length of line used for getline
+	ssize_t nread;										// bytes read for getLine
+	size_t linesToRead = 5;								// lines to read specified by -n (N)
+	size_t linesRead = 0;								// Number of lines read
+	line_node *head = malloc(sizeof(line_node));		// dummy head pointer for the linked-list
+	line_node *cur = head;								// cur pointer for traversing
 
 	if (argc > 4) 			// Too many arguments, exit
 	{
 		printf("Too many arguments!\n");
 		exit(EXIT_FAILURE);
 	}
-
-	// if new_tail is invoked by itself,
-	// read from stdin until ctrl-d
-	// and print the last 5 messages
-	if (argc == 1)
-	{	
-		while ((nread = getline(&line, &len, stdin)) != -1)			// keep reading until eof or error
-		{
-			// While you have valid input, allocate these "nodes" so that
-			//  you store the line and information about the line.
-			// This linked list is prepared so that you can traverse it later.
-			line_node *newNode = malloc(sizeof(line_node));			
-			newNode->gotLine = line;
-			newNode->length = len;
-			cur->next = newNode;
-			cur = cur->next;
-			line = NULL;
-			linesRead++;
-			
-			// if you exceed the # of lines to read,
-			// deallocate the head, but make sure to adjust accordingly
-			if (linesRead >= linesToRead) {
-				head = deallocateLineNode(head);
-			}	
-		}
-	
-		// re-adjust cur pointer if there are less than 5 lines read
-		//  This is because we have a dummy node at head in the beginning
-		if (linesRead < linesToRead)
-			cur = head->next;
-		else
-			cur = head;
-		
-		for (int i = 0; i < linesToRead; i++) {
-			if (!cur) 
-				return 0;
-
-			fwrite(cur->gotLine, cur->length, 1, stdout);
-			cur = deallocateLineNode(cur);
-		}	
-		
-	}	
 	else  
 	{
+		// if new_tail is invoked by itself,
+		// read from stdin until ctrl-d
+		// and print the last 5 messages
+		if (argc == 1)
+			fs = stdin;
+		
+		// Loop through each argument to get correct idea of what the arguments should be.
+		// If argc == 1, this loop will not trigger
 		for (int i = 1; i < argc; i++) 
 		{
 			if (strcmp(argv[i], "-n") == 0 && i+1 < argc) 
@@ -101,6 +64,8 @@ int main(int argc, char*argv[]) {
 			printf("Error opening file specified.\n");
 			exit(EXIT_FAILURE);
 		}
+
+		// ************ BUFFERING *************** // 
 		while ((nread = getline(&line, &len, fs)) != -1) 			// keep reading until eof or error
 		{
 			// While you have valid input, allocate these "nodes" so that
@@ -127,7 +92,9 @@ int main(int argc, char*argv[]) {
 			cur = head->next;
 		else
 			cur = head;
-		
+	
+		// Print out the results in the buffer
+		// Also free up memory as you go
 		for (int i = 0; i < linesToRead; i++) {
 			if (!cur) 
 				return 0;
@@ -141,12 +108,10 @@ int main(int argc, char*argv[]) {
 	return 0;
 }
 
-void deallocate(line_node *cur){
-	while(cur) {
-		cur = deallocateLineNode(cur);
-	}
-}
-
+// deallocateLineNode takes in a line_node pointer
+//  and deallocates dynamic memory inside the struct
+//  as well as the node itself.
+// Returns: the pointer pointing to the next node of the passed in node.
 line_node* deallocateLineNode(line_node *node) {
 	line_node *ret = node->next;
 	free(node->gotLine);
